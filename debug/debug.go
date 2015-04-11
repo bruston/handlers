f5ad84dbbdb23cf97a93327292f356b7ed1fc798
Benjamin Ruston <benjy.ruston@gmail.com>
2015-04-11 19:31:25 +0100

    WriteHeader should have a pointer receiver

--- debug/debug.go
+++ debug/debug.go
@@ -11,14 +11,14 @@ type debugResponsewriter struct {
 	code int
 }
 
-func (d debugResponsewriter) WriteHeader(n int) {
+func (d *debugResponsewriter) WriteHeader(n int) {
 	d.ResponseWriter.WriteHeader(n)
 	d.code = n
 }
 
 func New(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		drw := debugResponsewriter{w, 0}
+		drw := &debugResponsewriter{w, 0}
 		start := time.Now()
 		h.ServeHTTP(drw, r)
 		log.Printf("%s - %s - %s - %s - %d - %s", time.Since(start).String(), r.RemoteAddr, r.Method, r.RequestURI, drw.code, r.UserAgent())
