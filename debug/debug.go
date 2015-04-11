19a5884dbd1b7881671470954c49447206d7c301
Benjamin Ruston <benjy.ruston@gmail.com>
2015-04-11 20:49:03 +0100

    Fixed typo

--- debug/debug.go
+++ debug/debug.go
@@ -6,18 +6,18 @@ import (
 	"time"
 )
 
-type debugResponsewriter struct {
+type debugResponseWriter struct {
 	http.ResponseWriter
 	code    int
 	written int64
 }
 
-func (d *debugResponsewriter) WriteHeader(n int) {
+func (d *debugResponseWriter) WriteHeader(n int) {
 	d.ResponseWriter.WriteHeader(n)
 	d.code = n
 }
 
-func (d *debugResponsewriter) Write(b []byte) (int, error) {
+func (d *debugResponseWriter) Write(b []byte) (int, error) {
 	n, err := d.ResponseWriter.Write(b)
 	d.written += int64(n)
 	return n, err
@@ -25,7 +25,7 @@ func (d *debugResponsewriter) Write(b []byte) (int, error) {
 
 func New(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		drw := &debugResponsewriter{w, 0, 0}
+		drw := &debugResponseWriter{w, 0, 0}
 		start := time.Now()
 		h.ServeHTTP(drw, r)
 		log.Printf("%s - %s - %s - %s - %d - %d - %s", time.Since(start).String(), r.RemoteAddr, r.Method, r.RequestURI, drw.code, drw.written, r.UserAgent())
